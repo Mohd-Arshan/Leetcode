5ee3e16bb6823f4c5a7674ffea31e966fe944ddc
Mohd Arshan <147928409+Mohd-Arshan@users.noreply.github.com>
2026-08-10 20:19:56 +0530

    Time: 310 ms (9.21%), Space: 31.3 MB (10.75%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,39 +1,30 @@
 class Solution {
-private:
-    int dp[100001][2];
-    bool rec(int n,bool turn, vector<int>& square){
-        if(n==0) return !turn;
-
-        if(dp[n][turn] != -1) return dp[n][turn];
-        bool ans;
-        if(turn){
-            ans = false;
-            for(int it:square){
-                if(it>n) break;
-
-                ans = ans || rec(n-it,false,square);
-            }
-        }
-        else{
-            ans = true;
-
-            for(int it:square){
-                if(it>n) break;
-
-                ans = ans && rec(n-it,true,square);
-            }
-        }
-
-        return dp[n][turn] = ans;
-    }
 public:
     bool winnerSquareGame(int n) {
         vector<int> square;
         for(int k=1; k*k<=1e5; k++){
             square.push_back(k*k);
         }
+        vector<vector<int>> dp(n+1,vector<int>(2));
+        dp[0][0] = false;
+        dp[0][1] = true;
 
-        memset(dp,-1,sizeof(dp));
-        return rec(n,true,square);
+        bool turn = true;
+        for(int i=1; i<=n; i++){
+
+            dp[i][0] = false;
+            for(int it:square){
+                if(i - it < 0) break;
+                dp[i][0] = (dp[i][0] || dp[i-it][1]);
+            }
+
+            dp[i][1] = true;
+            for(int it:square){
+                if(i-it < 0) break;
+                dp[i][1] = (dp[i][1] && dp[i-it][0]);
+            }
+        }
+
+        return dp[n][0];
     }
 };
